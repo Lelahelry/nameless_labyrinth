daf1d2b8122fa856251d3ec6aee08c43dd0eee2d
Clem <clementine.carbon@gmail.com>
2023-05-03 17:18:15 +0200

    restauration

--- graphics.py
+++ graphics.py
@@ -10,7 +10,7 @@ class Game_window():
     def __init__(self):
         self.root = ctk.CTk()
         self.root.title("Labyrinth - the aMAZEing game")
-        self.root.geometry("300x350")
+        self.root.geometry("1000x600")
         # management of the communication with the game class
             
         ########################################
@@ -18,9 +18,6 @@ class Game_window():
         ########################################
         # graphical elements
         self.folder = "./textures" # adresse 
-        # canva size
-        self.f_graph_height = 2000
-        self.f_graph_width = 2000
 
         # display window setting
         self.f_graph = None
@@ -32,7 +29,7 @@ class Game_window():
 
     def widgets_creation(self, root):
         # text
-        self.welcome = ctk.CTkLabel(root, height = 160, text = "Welcome to the aMAZEing Labyrinth - Software version !", text_color = "DodgerBlue4", font = ('Calibri', 28, 'bold'))
+        self.welcome = ctk.CTkLabel(root, height = 110, text = "Welcome to the aMAZEing Labyrinth - Software version !", text_color = "DodgerBlue4", font = ('Calibri', 28, 'bold'))
         self.welcome.pack(side = tk.TOP)
 
         self.label = ctk.CTkLabel(root, height = 40, text = "Choose the number of players and enter their names :", font = ('Calibri', 20))
@@ -56,8 +53,7 @@ class Game_window():
         self.button_launch = ctk.CTkButton(self.root, text = "Start game", corner_radius = 8, height = 60, width = 15, fg_color = "goldenrod", hover_color = "DodgerBlue4", font = ('Calibri', 20))
         self.button_launch.bind('<Button-1>', self.game_launch)
         self.button_launch.pack(side = ctk.BOTTOM, fill = 'x')
-      
-
+ 
     def add_playernames(self, event):
         """adds entry bars to get player names"""
         length = self.player_number.get()
@@ -77,32 +73,64 @@ class Game_window():
             for i in range(diff):
                 self.playernames_e.pop().pack_forget()
            
-
     def game_launch(self, message):
         """creation of the window"""
         # find the player name
+        self.get_playernames()
+        
+        # init the game through controller
+        #self.controller.init_control()
+
+        # creation of the graphic window
+        self.graphic_window() 
+        
+    #callback
+    def get_playernames(self):
+        """recovers the player's name
+        no input
+        no output"""
         for i in range(len(self.playernames_e)):
             name = str(self.playernames_e[i].get())
             if name == "":
-                    name = "Player" + str(i+1)
-                    self.playernames.append(name)
-        # init the game through message
-        # creation of the graphic window
+                name = "Player" + str(i+1)
+            self.playernames.append(name)
+    
+    def graphic_window(self):
+        """creates graphic window for current game display
+        no input
+        no output"""
         if (self.f_graph == None) :
             self.f_graph = ctk.CTkToplevel(self.root)
-            self.f_graph.width = self.f_graph_width
-            self.f_graph.height = self.f_graph_height
-            # canvas for the board
-            self.f_graph.canvas_board = tk.Canvas(self.f_graph, height = self.f_graph.height, width = self.f_graph.width - 800)
-            self.background = tk.PhotoImage(file = self.folder + '\\board.png')
-            self.item = self.f_graph.canvas_board.create_image(500, 300, image = self.background, anchor = 'c')
-            """self.item.resize((1000,1000))"""
-            self.f_graph.canvas_board.lower(self.item)
-            self.f_graph.canvas_board.pack(side = tk.LEFT)
-            # canvas for the card of the current objective
-            self.f_graph.canvas_card = tk.Canvas(self.f_graph, height = self.f_graph.height/2, width = 800, bg = "magenta")
-            self.f_graph.canvas_card.pack(side = tk.RIGHT)
-        # self.place_objects()
+            self.f_graph.title('Labyrinth - Current game')
+            self.f_graph.geometry("1000x600")
+            
+            
+            #bouton "mon tour est fini"?
+            self.button_done = ctk.CTkButton(self.f_graph, text = "My turn is over.", corner_radius = 8, height = 30, width = 15, fg_color = "goldenrod", hover_color = "DodgerBlue4", font = ('Calibri', 20))
+            #self.button_done.bind('<Button-1>', self.turn_over())
+            self.button_done.pack(side = ctk.BOTTOM, fill = 'x')
+            
+            
+            self.image_dict = {}#stock all 50 tiles as (bg, fg, pawn) in grid size
+
+            self.canvas_for_board()
+           
+            self.slide_tiles_buttons()
+            
+            self.canvas_for_objective() 
+            
+            self.text_area()
+            
+            self.canvas_for_hand()
+
+            self.turn_tile_buttons()
+            
+            self.validate_buttons()    
+
+    def canvas_for_board(self):
+        """creates the canva for the board with the background
+        no input
+        no output"""
         
         self.canvas_board = tk.Canvas(self.f_graph, width = 1100, height = 1100)
         
@@ -341,14 +369,6 @@ class Game_window():
 
 
 
-récupération des positions de clic
-def affiche_info_ville(self,event):
-        self.text_area.delete("1.0", "end")
-        mouseX = event.x
-        mouseY = event.y
-        ident = self.f_graph.canevas.find_withtag("current")[0]
-        item_clicked = self.dicoSommetsGraphiques[ident]
-        self.text_area.insert(tk.INSERT,item_clicked)
     récupération des positions de clic
     def affiche_info_ville(self,event):
             self.text_area.delete("1.0", "end")
